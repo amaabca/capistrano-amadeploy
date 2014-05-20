1224b2b9ca53496819a3c89f2b8d2a22c8e21b14
Ryan Jones <ryan@system88.com>
2014-05-20 11:04:26 -0600

    remove campfire, add in hipchat hook (yellow for start deploy, green for finished

---
+++ lib/capistrano-deploy/hipchat.rb
@@ -0,0 +1,27 @@
+module CapistranoDeploy
+  module Hipchat
+    def self.load_into(configuration)
+      configuration.load do
+        namespace :hipchat do
+          require 'hipchat'
+
+          desc 'Set notification in hipchat for deployment start'
+          task :start_msg do
+            client = HipChat::Client.new(hipchat_token)
+            deploy_text = "#{git_config_user.chomp} is starting deploy of '#{app_name.upcase}' from branch '#{branch.upcase}' to #{current_stage.upcase}"
+
+            client[hipchat_room].send('Deploy', deploy_text, color: 'yellow')
+          end
+
+          desc 'Set notification in hipchat for deployment end'
+          task :end_msg do
+            client = HipChat::Client.new(hipchat_token)
+            deploy_text = "#{git_config_user.chomp} is starting deploy of '#{app_name.upcase}' from branch '#{branch.upcase}' to #{current_stage.upcase}"
+
+            client[hipchat_room].send('Deploy', deploy_text, color: 'green')
+          end
+        end
+      end
+    end
+  end
+end
